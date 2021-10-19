650f8cc09721fb686d6cb8a1d2fbf5fdee9b0d9c
callimarieYT <85613286+callimarieYT@users.noreply.github.com>
2021-10-18 20:52:28 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+token= "ODk5ODQzMTY2MTM0NDgwOTQ2.YW4qQQ.8aNTaViFD5C2AC67yHN07MG6FCs"
+prefix= "!"
+
+bot = commands.Bot(command_prefix=prefix)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('Looking over callimaries basement'))
+    print("The bot is online")
+
+
+@bot.command()
+async def embed(ctx, ch:discord.TextChannel):
+    a_color = discord.Colour.blue()
+    myembed = discord.Embed(title="Rules for callimarie's basement", color = a_color).set_author(name="callimarie")
+    myembed.add_field(name="Rules:", value="This is an anarchy server, so the only one is,", inline=False)
+    myembed.add_field(name="rule 1", value="Dont Break tos.", inline=False)
+    myembed.set_footer(text="Callimarie's basement")
+    
+    await ch.send(embed=myembed)   
+
+
+bot.run(token)
