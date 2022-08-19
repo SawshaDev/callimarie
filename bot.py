4857e7c747f7de9be23c537109ed4860dc4b8307
SawshaDev <contactcallimarie@gmail.com>
2022-08-18 22:26:01 -0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+from config import token
+
+class Callimarie(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=">", intents=discord.Intents.all())
+
+        
+    async def run(self):
+        await self.start("")
