a690b386cf5ce550e17c87784d05e92ecfe04b4b
SawshaDev <85613286+SawshaDev@users.noreply.github.com>
2022-09-18 15:19:45 -0700

    Update main.py
    stfu

--- main.py
+++ main.py
@@ -30,14 +30,6 @@ async def on_ready():
     print(f"The bot is online")
 
 
-@bot.command()
-async def embed(ctx, ch:discord.TextChannel):
-    a_color = discord.Colour.blue()
-    myembed = discord.Embed(title="Rules for callimarie's basement", color = a_color).set_author(name="callimarie")
-    myembed.add_field(name="Rules:", value="This is an anarchy server, so the only one is,", inline=False)
-    myembed.add_field(name="rule 1", value="Dont Break tos.", inline=False)
-    myembed.set_footer(text="Callimarie's basement")
-    await ch.send(embed=myembed)   
 
 @bot.command()
 async def ping(ctx):
